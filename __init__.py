ad120acee2d1add6e08251ab518a533f80064654
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-01-30 18:36:23 -0800

    Update parsing to use OCP Plugin (#8)
    * Refactor metadata parsing to use OVOS plugin
    Add logging in skill to troubleshoot
    
    * Update skill.json
    
    * Debug logging and better exception handling in library init
    
    * Make genre tags optional to handle default m4a tags
    
    * Troubleshooting playback
    
    * Troubleshooting meta init
    
    * Update OCP dependency to include local file fix
    
    * Update skill.json
    
    * Troubleshooting dependency resolution
    
    * Update skill.json
    
    * Loosen OCP dependency
    
    * Update skill.json
    
    ---------
    
    Co-authored-by: NeonDaniel <NeonDaniel@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -54,6 +54,7 @@ class LocalMusicSkill(OVOSCommonPlaybackSkill):
     @property
     def music_library(self):
         if not self._music_library:
+            LOG.info(f"Initializing music library at: {self.music_dir}")
             self._music_library = MusicLibrary(self.music_dir,
                                                self.file_system.path)
         return self._music_library
@@ -78,7 +79,8 @@ class LocalMusicSkill(OVOSCommonPlaybackSkill):
             if len(all_songs) > 50:
                 all_songs = sample(self.music_library.all_songs, 50)
             results = self._tracks_to_search_results(all_songs, score)
-            LOG.debug(f"Returning all songs ({len(results)}) with score={score}")
+            LOG.debug(f"Returning all songs with score={score}")
+        LOG.info(f"Returning {len(results)} results")  # conf 65
         return results
 
     def search_artist(self, phrase, media_type=MediaType.GENERIC) -> List[dict]:
